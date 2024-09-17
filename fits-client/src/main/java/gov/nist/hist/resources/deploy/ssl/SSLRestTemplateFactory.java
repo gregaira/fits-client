e72a59436b4c37b244d97f95083fca1662a92530
Greg Noe <gnoe@immregistries.org>
2024-09-17 15:57:18 -0500

    Upgrade to Spring 6 and httpclient5

--- fits-client/src/main/java/gov/nist/hist/resources/deploy/ssl/SSLRestTemplateFactory.java
+++ fits-client/src/main/java/gov/nist/hist/resources/deploy/ssl/SSLRestTemplateFactory.java
@@ -3,37 +3,46 @@ package gov.nist.hist.resources.deploy.ssl;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContextBuilder;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
+import javax.net.ssl.SSLContext;
+
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.BasicHttpClientConnectionManager;
+import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
+import org.apache.hc.client5.http.socket.PlainConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.core5.http.config.Registry;
+import org.apache.hc.core5.http.config.RegistryBuilder;
+import org.apache.hc.core5.ssl.SSLContexts;
+import org.apache.hc.core5.ssl.TrustStrategy;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.RestTemplate;
-
-import gov.nist.hit.resources.deploy.config.RestTemplateHeaderModifierInterceptor;
 
 public class SSLRestTemplateFactory {
 
-	@SuppressWarnings("deprecation")
+	// https://www.baeldung.com/httpclient-ssl
 	public static RestTemplate createSSLRestTemplate() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
+		final TrustStrategy acceptingTrustStrategy = (cert, authType) -> true;
+		final SSLContext sslContext = SSLContexts.custom()
+			.loadTrustMaterial(null, acceptingTrustStrategy)
+			.build();
+		final SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+		final Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory> create()
+			.register("https", sslsf)
+			.register("http", new PlainConnectionSocketFactory())
+			.build();
 
-		SSLConnectionSocketFactory socketFactory = 
-				new SSLConnectionSocketFactory(new SSLContextBuilder().loadTrustMaterial(null, new TrustAllStrategy()).build(), SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
-		HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory).setHostnameVerifier(SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER).build();
-		HttpComponentsClientHttpRequestFactory fct = new HttpComponentsClientHttpRequestFactory(httpClient);
-		RestTemplate tmpl = new RestTemplate(fct);
-		List<ClientHttpRequestInterceptor> interceptors = tmpl.getInterceptors();
-		if (CollectionUtils.isEmpty(interceptors)) {
-			interceptors = new ArrayList<>();
-		}
-		interceptors.add(new RestTemplateHeaderModifierInterceptor());
-		tmpl.setInterceptors(interceptors);
-		return tmpl;
+		final BasicHttpClientConnectionManager connectionManager = 
+			new BasicHttpClientConnectionManager(socketFactoryRegistry);
+		final CloseableHttpClient httpClient = HttpClients.custom()
+			.setConnectionManager(connectionManager)
+			.build();
 
+		final HttpComponentsClientHttpRequestFactory requestFactory =
+			new HttpComponentsClientHttpRequestFactory(httpClient);
+		
+		return new RestTemplate(requestFactory);
 	}
 }
